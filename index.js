c6715770121fb6d72b68094bba4bba37cb92be2e
jiuzhou.zhang <jiuzhou.zhang@fengjr.com>
2017-12-22 13:33:21 +0800

    1、增加deleteComponent方法 2、利用immutable减少数据修改

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 'use strict'
 
-import { OrderedMap, Map } from 'immutable'
+import { OrderedMap, Map, is, fromJS } from 'immutable'
 
 class ListDataSource {
   _display = false
@@ -15,6 +15,8 @@ class ListDataSource {
     this._display = true
   }
 
+  _lastData
+
   get data () {
     if (!this._display) {
       return null
@@ -24,7 +26,69 @@ class ListDataSource {
       this._list = []
       this._getList(this._root, this._list)
     }
+
     return this._list
+  }
+
+  setHidden (keypath, hidden) {
+    const item = this._getItem(keypath)
+    if (item) {
+      this._setRoot(this._setItem(keypath, null, item.set('hidden', hidden)))
+    }
+  }
+
+  getItemCount (keypath = null) {
+    return this._numberOfItems(keypath)
+  }
+
+  addGroup (keypath, { hidden = false } = {}) {
+    const item = this._getItem(keypath)
+    if (!item) {
+      this._setRoot(this._setItem(keypath, 'group', {hidden, 'children': OrderedMap()}))
+    }
+  }
+
+  resetGroup (keypath = null) {
+    if (!keypath) {
+      this._setRoot(OrderedMap())
+    } else {
+      const item = this._getItem(keypath)
+      if (item && item.get('type') === 'group') {
+        this._setRoot(this._setItem(keypath, null, item.set('children', OrderedMap())))
+      }
+    }
+  }
+
+  getComponent (keypath) {
+    const item = this._getItem(keypath)
+    if (item && item.get('type') === 'component') {
+      return item
+    }
+  }
+
+  addOrUpdateComponent (keypath, {component = null, hidden = false, ...props}, position = 'end') {
+    const item = this._getItem(keypath)
+
+    props.key = keypath.join('.')
+
+    if (item) { // update
+      if (item.get('type') === 'component') {
+        this._setRoot(this._setItem(keypath, 'component', {props}))
+      }
+    } else {
+      if (keypath.length > 1) {
+        const groupItem = this._getItem(keypath.slice(0, -1))
+        if (groupItem && groupItem.get('type') === 'group') {
+          this._setRoot(this._setItem(keypath, 'component', {hidden, component, props}, position))
+        }
+      } else {
+        this._setRoot(this._setItem(keypath, 'component', {hidden, component, props}, position))
+      }
+    }
+  }
+
+  deleteComponent (keypath) {
+    this._setRoot(this._deleteItem(keypath, 'component'))
   }
 
   _getList (map, list) {
@@ -42,53 +106,33 @@ class ListDataSource {
     }
   }
 
-  setHidden (keypath, hidden) {
-    const item = this._getItem(keypath)
-    if (item) {
-      this._setRoot(this._setItem(keypath, null, item.set('hidden', hidden)))
-    }
-  }
-
-  addGroup (keypath, { hidden = false } = {}) {
-    const item = this._getItem(keypath)
-    if (!item) {
-      this._setRoot(this._setItem(keypath, 'group', { hidden }))
-    }
-  }
-
-  resetGroup (keypath) {
-    const item = this._getItem(keypath)
-    if (item && item.get('type') === 'group') {
-      this._setRoot(this._setItem(keypath, null, item.set('children', OrderedMap())))
-    }
-  }
-
-  addOrUpdateComponent (keypath, { component = null, hidden = false, ...props }) {
-    const item = this._getItem(keypath)
-
-    if (item) { // update
-      if (item.get('type') === 'component') {
-        let newItem = item
-        newItem = newItem.set('props', newItem.get('props').merge(Map(props)))
-        this._setRoot(this._setItem(keypath, null, newItem))
-      }
-    } else {
-      if (keypath.length > 1) {
-        const groupItem = this._getItem(keypath.slice(0, -1))
-        if (groupItem && groupItem.get('type') === 'group') {
-          this._setRoot(this._setItem(keypath, 'component', { hidden, component, props: Map(props) }))
-        }
-      } else {
-        this._setRoot(this._setItem(keypath, 'component', { hidden, component, props: Map(props) }))
-      }
-    }
-  }
-
   _setRoot (newRoot) {
     if (newRoot !== this._root) {
       this._root = newRoot
       this._list = null
     }
+  }
+
+  _numberOfItems (keypath = null, map = this._root) {
+    if (!keypath) {
+      return this._root.size
+    } else {
+      const key = keypath[0]
+
+      if (keypath.length > 1) {
+        const subMap = map.get(key)
+        if (subMap && subMap.get('type') === 'group') {
+          return this._numberOfItems(keypath.slice(1), subMap.get('children'))
+        }
+      } else {
+        let item = map.get(key)
+        if (item.get('type') === 'group') {
+          return item.get('children').size
+        }
+      }
+    }
+
+    return 0
   }
 
   _getItem (keypath, map = this._root) {
@@ -106,25 +150,66 @@ class ListDataSource {
     }
   }
 
-  _setItem (keypath, type, attrs = {}, map = this._root) {
+  _setItem (keypath, type, attrs = {}, position = 'end', map = this._root) {
     const key = keypath[0]
 
     if (keypath.length > 1) {
       const subMap = map.get(key)
       if (subMap) {
         if (subMap.get('type') === 'group') {
-          return map.update(key, v => {
-            v = v.set('children', this._setItem(keypath.slice(1), type, attrs, subMap.get('children')))
+          map = map.update(key, v => {
+            v = v.set('children', this._setItem(keypath.slice(1), type, attrs, position, subMap.get('children')))
             return v
           })
         }
       }
     } else {
-      let item = !Map.isMap(attrs) ? new Map({ type, ...attrs }) : attrs
-      if (type === 'group') {
-        item = item.set('children', OrderedMap())
+      let item
+      if (Map.isMap(attrs)) {
+        item = attrs
+      } else {
+        item = map.get(key)
+        if (item) {
+          item = item.mergeWith(
+            (oldVal, newVal) => {
+              const mergedVal = {...oldVal, ...newVal}
+              return is(fromJS(oldVal), fromJS(mergedVal)) ? oldVal : mergedVal
+            },
+            Map(attrs))
+        } else {
+          item = new Map({ type, ...attrs })
+        }
       }
-      return map.set(key, item)
+
+      switch (position) {
+        case 'end':
+          map = map.set(key, item)
+          break
+        case 'front':
+          map = OrderedMap({[key]: item}).concat(map)
+          break
+      }
+    }
+
+    return map
+  }
+
+  _deleteItem (keypath, type, map = this._root) {
+    const key = keypath[0]
+
+    if (keypath.length > 1) {
+      const subMap = map.get(key)
+      if (subMap) {
+        if (subMap.get('type') === 'group') {
+          map = map.update(key, v => v.set('children',
+            this._deleteItem(keypath.slice(1), type, subMap.get('children'))))
+        }
+      }
+    } else {
+      let item = map.get(key)
+      if (item.get('type') === type) {
+        map = map.remove(key)
+      }
     }
 
     return map
